b27986a8519a93e91c3a59716b31aa2f1c6b53c5
Harshada Chavan <harshadachavhan2005@gmail.com>
2026-06-13 12:51:30 +0530

    mongodb connected

--- server/server.js
+++ server/server.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
 
 const app = express();
 
@@ -10,6 +15,8 @@ app.get("/", (req, res) => {
   res.send("ProjectSphere AI Backend Running");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
