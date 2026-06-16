e306af28a3ba364a4f775bbf7849ef4396752dad
Harshada Chavan <harshadachavhan2005@gmail.com>
2026-06-16 18:28:23 +0530

    jwt token

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -81,7 +81,7 @@ const loginUser = async (req, res) => {
       user: {
         _id: user._id,
         name: user.name,
-        email: user.email,
+        email: user.email,                   
         role: user.role,
       },
     });
