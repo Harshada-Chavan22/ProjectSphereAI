d9e1a3ca9d31695be2ff4958c40f7e4df80ca06b
Harshada Chavan <harshadachavhan2005@gmail.com>
2026-06-17 23:55:55 +0530

    port is 5000

--- server/server.js
+++ server/server.js
@@ -19,6 +19,7 @@ app.get("/", (req, res) => {
 });
 //this is a port so the browser can access the server, it will use the port defined in the environment variable or default to 5000 if not set
 //so the server can be accessed from the browser at http://localhost:5000 or the port defined in the environment variable
+//this is useful for deployment, as the port can be set in the environment variable and the server will use that port instead of the default 5000 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
