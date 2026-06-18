aeff227cf9967c400af37cd25dd6261541a57703
Harshada Chavan <harshadachavhan2005@gmail.com>
2026-06-19 00:15:39 +0530

    create Task API

---
+++ server/controllers/taskController.js
@@ -0,0 +1,33 @@
+const Task = require("../models/Task");
+
+const createTask = async (req, res) => {
+  try {
+    const {
+      title,
+      description,
+      projectId,
+      priority,
+      deadline,
+    } = req.body;
+
+    const task = await Task.create({
+      title,
+      description,
+      projectId,
+      assignedTo: req.user.id,
+      priority,
+      deadline,
+    });
+
+    res.status(201).json({
+      message: "Task created successfully",
+      task,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+module.exports = { createTask };
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const taskRoutes = require("./routes/taskRoutes");
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
@@ -9,11 +10,13 @@ connectDB();
 const app = express();
 const authRoutes = require("./routes/authRoutes");
 const projectRoutes = require("./routes/projectRoutes");
+//app.use("/api/tasks", taskRoutes);
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
+app.use("/api/tasks", taskRoutes);
 app.get("/", (req, res) => {
   res.send("ProjectSphere AI Backend Running");
 });
