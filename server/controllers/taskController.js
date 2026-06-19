ef0d5c2d90dd1cc76267577be7217388fe778b65
Harshada Chavan <harshadachavhan2005@gmail.com>
2026-06-19 23:34:02 +0530

    Task By Project is working

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -2,13 +2,7 @@ const Task = require("../models/Task");
 
 const createTask = async (req, res) => {
   try {
-    const {
-      title,
-      description,
-      projectId,
-      priority,
-      deadline,
-    } = req.body;
+    const { title, description, projectId, priority, deadline } = req.body;
 
     const task = await Task.create({
       title,
@@ -30,4 +24,43 @@ const createTask = async (req, res) => {
   }
 };
 
-module.exports = { createTask };
+const getTasksByProject = async (req, res) => {
+  try {
+    const tasks = await Task.find({
+      projectId: req.params.projectId,
+    });
+
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+const updateTaskStatus = async (req, res) => {
+  try {
+    const { status } = req.body;
+
+    const task = await Task.findByIdAndUpdate(
+      req.params.id,
+      { status },
+      { new: true }
+    );
+
+    res.status(200).json({
+      message: "Task updated successfully",
+      task,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+module.exports = {
+  createTask,
+  getTasksByProject,
+  updateTaskStatus,
+};
--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const router = express.Router();
 
-const { createTask } = require("../controllers/taskController");
-const { protect } = require("../middleware/authMiddleware");
+const {
+  createTask,
+  getTasksByProject,
+  updateTaskStatus,
+} = require("../controllers/taskController");
+
+const {
+  protect,
+} = require("../middleware/authMiddleware");
 
 router.post("/", protect, createTask);
+
+router.get(
+  "/project/:projectId",
+  protect,
+  getTasksByProject
+);
+
+router.put(
+  "/:id/status",
+  protect,
+  updateTaskStatus
+);
 
 module.exports = router;
