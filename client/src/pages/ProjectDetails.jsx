988ab3935551a19b9349bc17b0ad8c9aeeb6de99
Harshada Chavan <harshadachavhan2005@gmail.com>
2026-06-26 23:24:09 +0530

    Create Task Working

--- client/src/pages/ProjectDetails.jsx
+++ client/src/pages/ProjectDetails.jsx
@@ -1,3 +1,5 @@
+
+
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import API from "../services/api";
@@ -6,6 +8,11 @@ function ProjectDetails() {
   const { id } = useParams();
 
   const [project, setProject] = useState(null);
+
+  const [taskTitle, setTaskTitle] = useState("");
+const [taskDescription, setTaskDescription] = useState("");
+const [priority, setPriority] = useState("Medium");
+const [deadline, setDeadline] = useState("");
 
   useEffect(() => {
     fetchProject();
@@ -31,6 +38,38 @@ function ProjectDetails() {
     return <h1>Loading...</h1>;
   }
 
+  const createTask = async () => {
+  try {
+    const token = localStorage.getItem("token");
+
+    await API.post(
+      "/tasks",
+      {
+        title: taskTitle,
+        description: taskDescription,
+        projectId: id,
+        priority,
+        deadline,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    alert("Task Created Successfully!");
+
+    setTaskTitle("");
+    setTaskDescription("");
+    setPriority("Medium");
+    setDeadline("");
+
+  } catch (error) {
+    console.log(error);
+    alert("Failed to create task");
+  }
+};
   return (
     <div className="min-h-screen bg-gray-100 p-8">
 
@@ -49,8 +88,55 @@ function ProjectDetails() {
         </span>
 
       </div>
+<div className="bg-white mt-8 p-6 rounded-2xl shadow-md">
 
+  <h2 className="text-2xl font-bold mb-6">
+    📋 Create New Task
+  </h2>
+
+  <input
+    type="text"
+    placeholder="Task Title"
+    value={taskTitle}
+    onChange={(e) => setTaskTitle(e.target.value)}
+    className="w-full p-3 border rounded-xl mb-4"
+  />
+
+  <textarea
+    placeholder="Task Description"
+    rows="4"
+    value={taskDescription}
+    onChange={(e) => setTaskDescription(e.target.value)}
+    className="w-full p-3 border rounded-xl mb-4"
+  />
+
+  <select
+    value={priority}
+    onChange={(e) => setPriority(e.target.value)}
+    className="w-full p-3 border rounded-xl mb-4"
+  >
+    <option>Low</option>
+    <option>Medium</option>
+    <option>High</option>
+  </select>
+
+  <input
+    type="date"
+    value={deadline}
+    onChange={(e) => setDeadline(e.target.value)}
+    className="w-full p-3 border rounded-xl mb-6"
+  />
+
+  <button
+    onClick={createTask}
+    className="bg-blue-600 text-white px-6 py-3 rounded-xl hover:bg-blue-700 transition"
+  >
+    + Create Task
+  </button>
+
+</div>
     </div>
+    
   );
 }
 
