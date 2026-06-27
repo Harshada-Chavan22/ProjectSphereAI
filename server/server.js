926ea7e5a3bbba28797674cd1e35f82a073ada08
Harshada Chavan <harshadachavhan2005@gmail.com>
2026-06-27 23:42:38 +0530

    Port number added

--- server/server.js
+++ server/server.js
@@ -26,5 +26,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);//5173 - port is used for user interface
 });
