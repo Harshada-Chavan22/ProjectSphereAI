adb302844160ffa4975cb5dc33a1b9020ed32710
Harshada Chavan <harshadachavhan2005@gmail.com>
2026-07-14 23:36:06 +0530

    removed delete button

--- client/src/pages/ProjectDetails.jsx
+++ client/src/pages/ProjectDetails.jsx
@@ -185,12 +185,7 @@ const updateStatus = async (taskId, status) => {
   >
     + Create Task
   </button>
-  <button
-  onClick={() => deleteTask(task._id)}
-  className="mt-4 bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg transition"
->
-  🗑 Delete
-</button>
+  
 </div>
 
 </div>
